427239a563785b136ef025113e9c838082a3ad24
Abhay Biramane <103833247+ABHAYB642@users.noreply.github.com>
2022-09-22 13:15:45 +0530

    Add files via upload

---
+++ Weather.py
@@ -0,0 +1,62 @@
+# import required modules
+from re import T
+import requests
+import json
+
+Api_key = "af7232b4c7324bf33032526e398ec5f0"
+Base_url = "http://api.openweathermap.org/data/2.5/weather?"
+city_name = input("Enter city name : ")
+
+
+complete_url = Base_url + "appid=" + Api_key + "&q=" + city_name
+
+
+response = requests.get(complete_url)
+
+
+x = response.json()
+
+# Now x contains list of nested dictionaries
+# Check the value of "cod" key is equal to
+# "404", means city is found otherwise,
+# city is not found
+if x["cod"] != "404":
+
+	# store the value of "main"
+	# key in variable y
+	y = x["main"]
+
+	# store the value corresponding
+	# to the "temp" key of y
+	current_temperature = y["temp"]
+
+	# store the value corresponding
+	# to the "pressure" key of y
+	current_pressure = y["pressure"]
+
+	# store the value corresponding
+	# to the "humidity" key of y
+	current_humidity = y["humidity"]
+
+	# store the value of "weather"
+	# key in variable z
+	z = x["weather"]
+
+	# store the value corresponding
+	# to the "description" key at
+	# the 0th index of z
+	weather_description = z[0]["description"]
+
+	# print following values
+	print(" Temperature = " +
+					str(current_temperature-273.15) +
+                
+		"\n atmospheric pressure (in hPa unit) = " +
+					str(current_pressure) +
+		"\n humidity (in percentage) = " +
+					str(current_humidity) +
+		"\n description = " +
+					str(weather_description))
+
+else:
+	print(" City Not Found ")
